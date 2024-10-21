9acb5b1d2787b59c730fb9ef39d3d69fe0c0434c
Divya <singhdivya10aug2002@gmail.com>
2024-10-21 21:17:57 +0530

    Updating Structure

--- Templates/Efficient_String_Building.cpp
+++
@@ -1,5 +0,0 @@
-string fn(vector<char>& arr) {
-    return string(arr.begin(), arr.end())
-}
-
-{% comment %} In JavaScript, benchmarking shows that concatenation with += is faster than using .join(). {% endcomment %}
