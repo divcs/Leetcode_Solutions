256a0f35ffad316c550367e814b70a7042594e8c
Tc-Anurag <23anuragsharma@gmail.com>
2024-10-24 10:32:13 +0530

    Question 14 has solved

---
+++ Questions/Easy/Q_14_longest_common_prefix/index.js
@@ -0,0 +1,14 @@
+var longestCommonPrefix = function (strs) {
+  strs.sort();
+  let a = strs[0];
+  let b = strs[strs.length - 1];
+  let c = "";
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] == b[i]) {
+      c += a[i];
+    } else {
+      break;
+    }
+  }
+  return c;
+};
