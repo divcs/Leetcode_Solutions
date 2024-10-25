436434dcec1d6cccb877a7045a6900e9a7679778
Tc-Anurag <129600042+OM2309@users.noreply.github.com>
2024-10-25 10:18:52 +0530

    Q-35 solve

---
+++ Questions/Easy/Q_35_Search_Insert_Postion/insert.js
@@ -0,0 +1,10 @@
+var searchInsert = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] >= target) {
+      return i;
+    }
+  }
+  return nums.length;
+};
+
+console.log(searchInsert([1, 2, 3], 3));
